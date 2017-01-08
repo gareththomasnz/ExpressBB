1be188779257f41651c38a5cc7c1953ba8865d5f
gareththomasnz <gareththomasnz@yahoo.com>
2017-01-08 13:01:50 +1300

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+## Synopsis
+
+Building Blocks of Express
+
+
+## Motivation
+
+Learn node & Express JS
+
+
+## Contributors
+
+This is a commercial course
+
+## License
+
+Not public domain 
---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get ('/', function(request, response){
+        response.write('Hello World');
+        response.end();
+        });
+
+app.get ('/blocks', function(request, response){
+        //var blocks = ['Fixed', 'Movable', 'Rotating'];
+        //response.json(blocks);
+        response.redirect(301, '/parts');
+        });
+
+app.listen(3000, function(){
+        console.log('Listening on port 3000');
+        });
