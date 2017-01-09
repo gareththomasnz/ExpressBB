2a3ce1986852a876ef019e5b20494397f035c49f
gareththomasnz <gareththomasnz@yahoo.com>
2017-01-10 08:02:24 +1300

    fifth commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
 var logger = require('./logger');
 app.use(logger);
 
@@ -21,6 +24,13 @@ var blocks = {
 var locations = {
         'Fixed' : 'First Floor', 'Movable': 'Second Floor', 'Rotating': 'Penthouse'
 };
+
+app.post('/blocks', parseUrlencoded, function(request, response){
+        var newBlock = request.body;
+        blocks[newBlock.name] = newBlock.description;
+        
+        response.status(201).json(newBlock.name);
+        });
 
 app.param('name', function(request, response, next){
         var name = request.params.name;
--- public/client.js
+++ public/client.js
@@ -1,12 +1,35 @@
 $(function(){
-        $.get('/blocks', appendToList);
-        
-        function appendToList(blocks){
-                var list = [];
+
+	//Make a get request to our express backend, it should return a list of blocks in JSON format
+	$.get('/blocks', appendToList);
+
+	function appendToList(blocks) {
+		var list = [];
+                var content, block;
                 for(var i in blocks){
-                        list.push($('<li>', { text: blocks[i]}));
+                        block = blocks[i];
+                        content = '<a href="/blocks/' +block+'">'+block+'</a>';
+                        list.push($('<li>', { html: content }));
                 }
-                $('.block-list').append(list);
-        }
+		//for(var i in blocks){
+			//Adds block names to the new list
+			//list.push($('<li>', { text: blocks[i]}));
+		//}
+		//Add our list to the HTML page
+		$('.block-list').append(list);
+	}
         
-        });
+        $('form').on('submit', function(event){
+                event.preventDefault();
+                var form = $(this);
+                var blockData = form.serialize();
+                
+                $.ajax({
+                        type: 'POST', url: '/blocks', data: blockData
+                        }).done(function(blockName){
+                        appendToList([blockName]);
+                        form.trigger('reset');
+                        });
+                
+                });
+});
