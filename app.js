116dc3c0a00efed9e480043a815e5e898e575103
gareththomasnz <gareththomasnz@yahoo.com>
2017-01-09 16:19:35 +1300

    third commit

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
 
-app.get ('/', function(request, response){
-        response.write('Hello World');
-        response.end();
-        });
+var logger = require('./logger');
+app.use(logger);
+
+//app.get ('/', function(request, response){
+        //response.write('Hello World');
+        //response.end();
+        //response.sendFile( + '/public/index.html');
+        //});
+        
+app.use(express.static('public'));
 
 app.get ('/blocks', function(request, response){
-        //var blocks = ['Fixed', 'Movable', 'Rotating'];
-        //response.json(blocks);
-        response.redirect(301, '/parts');
+        var blocks = ['Fixed', 'Movable', 'Rotating'];
+        response.json(blocks);
+        //response.redirect(301, '/parts');
         });
 
 app.listen(3000, function(){
---
+++ logger.js
@@ -0,0 +1,16 @@
+module.exports = function(request, response, next){
+        var start = +new Date();
+        var stream = process.stdout;
+        var url = request.url;
+        var method = request.method;
+        
+	response.on('finish', function(){
+		var duration = +new Date() - start;
+		message = method + ' to ' + url + '\ntook ' + duration + ' ms \n\n';
+		//Message in console
+		stream.write(message);
+	});
+        
+        
+        next();
+        };
